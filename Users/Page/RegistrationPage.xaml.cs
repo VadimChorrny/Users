2a7f3e3e15f45de52c870acbd81b34b5ae5b5194
VadimChorrny <chorrny228@gmail.com>
2021-07-12 20:03:06 +0300

    Add project files.

---
+++ Users/Page/RegistrationPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Users.Page
+{
+    /// <summary>
+    /// Interaction logic for RegistrationPage.xaml
+    /// </summary>
+    public partial class RegistrationPage
+    {
+
+        // provider factory
+        private DbProviderFactory fact = null;
+        // connection class to database
+        private DbConnection conn = null;
+        // data adapter for disconnected mode
+        private DbDataAdapter da = null;
+        // DataSet
+        private DataSet set = null;
+
+        private bool isAdmin;
+        public RegistrationPage()
+        {
+            InitializeComponent();
+            string cs = ConfigurationManager.ConnectionStrings["Users"].ConnectionString;
+            // create factory from provider name
+            fact = DbProviderFactories.GetFactory(ConfigurationManager.ConnectionStrings["Users"].ProviderName);
+            // create sql connection class
+            conn = fact.CreateConnection();
+            conn.ConnectionString = cs;
+        }
+
+        private void Registration(string login,string pass,bool isAdmin)
+        {
+            conn.Open();
+            string sql = "insert into Users (Login,Password,isAdmin) values (@login,@password,@admin)";
+            DbCommand cmd = fact.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.Connection = conn;
+
+            DbParameter param = fact.CreateParameter();
+            param.ParameterName = "@login";
+            param.DbType = DbType.String;
+            param.Value = login;
+            cmd.Parameters.Add(param);
+            
+            DbParameter param1 = fact.CreateParameter();
+            param1.ParameterName = "@password";
+            param1.DbType = DbType.String;
+            param1.Value = pass;
+            cmd.Parameters.Add(param1);
+            
+            DbParameter param2 = fact.CreateParameter();
+            param2.ParameterName = "@admin";
+            param2.DbType = DbType.Boolean;
+            param2.Value = isAdmin;
+            cmd.Parameters.Add(param2);
+
+            cmd.ExecuteScalar();
+            conn.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)checkboxIsAdmin.IsChecked)
+            {
+                isAdmin = true;
+            }
+            else
+            {
+                isAdmin = false;
+            }
+            Registration(txtLogin.Text, txtPassword.Text,isAdmin);
+        }
+    }
+}
---
+++ Users/Page/UsersPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Users.Page
+{
+    /// <summary>
+    /// Interaction logic for UsersPage.xaml
+    /// </summary>
+    public partial class UsersPage 
+    {
+        // provider factory
+        private DbProviderFactory fact = null;
+        // connection class to database
+        private DbConnection conn = null;
+        // data adapter for disconnected mode
+        private DbDataAdapter da = null;
+        // DataSet
+        private DataSet set = null;
+
+        public UsersPage()
+        {
+            InitializeComponent();
+            // read connection string from config file
+            string cs = ConfigurationManager.ConnectionStrings["Users"].ConnectionString;
+            // create factory from provider name
+            fact = DbProviderFactories.GetFactory(ConfigurationManager.ConnectionStrings["Users"].ProviderName);
+
+            // create sql connection class
+            conn = fact.CreateConnection();
+            conn.ConnectionString = cs;
+        }
+
+        private void LoadAllUsers()
+        {
+            try
+            {
+                // query for select data
+                string sql = "SELECT * FROM Users;";
+                // create data adapter
+                da = fact.CreateDataAdapter();
+                da.SelectCommand = conn.CreateCommand();
+                da.SelectCommand.CommandText = sql;
+                // create command builder for auto generate insert, update and delete queries
+                var builder = fact.CreateCommandBuilder();
+                builder.DataAdapter = da;
+
+                // create empty DataSet
+                set = new DataSet();
+                // execute select query on server and put data to DataSet
+                da.Fill(set);
+
+                // bind table to DataGrid
+                dataGrid.ItemsSource = set.Tables[0].DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
